b5ce8a86ed8756c3e29184c4bf720668a1603aff
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-20 20:57:31 -0300

    Operadores

--- Program.cs
+++ Program.cs
@@ -1,19 +1,59 @@
 ﻿using fundamentosdotnet.Models;
 
 
-int a = 10;
-int b = 20;
+double a = 4 / 2 + 2;
 
-int c = a + b;
+//Console.WriteLine(a);
 
-//c = c + 5; // 30 + 5
+//int a = 5;
+//double b = a;
 
-//c += 5;
 
-//C = C - 5
-c -= 5;
+// int a = 5;
+// long b = a;
 
-Console.WriteLine(c);
+// Console.WriteLine(b);
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+
+
+
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+//Cast - Casting
+//int a = Convert.ToInt32("5");
+
+
+// int a = int.Parse("5");
+
+// Console.WriteLine(a);
+
+
+
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c = c + 5; // 30 + 5
+
+// c += 5;
+
+// C = C - 5
+// c -= 5;
+
+// Console.WriteLine(c);
 
 
 
