7a9a6e06accdbafdade3364411e527a882976812
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-20 17:02:09 -0300

    Alteração date

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
 ﻿using fundamentosdotnet.Models;
 
 
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+
+
+
+
+//string apresentar = "Olá, seja bem vindo";
+
+//int quantidade = 1;
+
+//double altura = 1.80;
+
+//decimal preco = 1.80M;
+
+//bool condicao = true;
+
+//Console.WriteLine(apresentar);
+//Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+//Console.WriteLine("Valor da variável quantidade: "+ altura);
+//Console.WriteLine("Valor da variável quantidade: "+ preco);
+//Console.WriteLine("Valor da variável quantidade: "+ condicao);
+
 
 
 
