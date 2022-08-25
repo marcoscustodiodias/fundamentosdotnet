c8ad6947168ec31c2ca3eaaa7c985110122ac607
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-24 21:41:49 -0300

    Aprendendo if e else no C#

--- Program.cs
+++ Program.cs
@@ -14,6 +14,18 @@
 
 
 
+// bool choveu = false;
+// bool estaTarde = false;
+
+// if (!choveu && !estaTarde)
+// {
+//     Console.WriteLine("Vou pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("Vou pedalar um outro dia.");
+// }
+
 
 
 
