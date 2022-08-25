2e53e8045b2ca1a7582f0d673f4278828e855aca
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-24 21:12:36 -0300

    Apredendo Operador && em C#

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
-﻿
+﻿using fundamentosdotnet.Models;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool possuiPresencaMinima = true;
+// double media = 7.5;
+
+// if (possuiPresencaMinima && media >= 7)
+// {
+//     Console.WriteLine("Aprovado");
+// }
+// else
+// {
+//     Console.WriteLine("Reprovado");
+// }
 
 
 
