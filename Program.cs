c0f91997b1d2467da4c5b1147be38cdb489a8403
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-25 20:51:03 -0300

    Calculadora em C#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿using fundamentosdotnet.Models;
 
 
+Calculadora calc = new Calculadora();
+
+calc.Somar(10,30);
+calc.Subtrair(10,50);
+calc.Multiplicar(15,45);
+calc.Dividir(2,2);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
