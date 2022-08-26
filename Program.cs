b8965cf029266688c17d7a85f938fa1671f073d3
Marcos Dias <marcosdias.pvai@gmail.com>
2022-08-26 15:17:28 -0300

    Aprendendo construir um menu em C#

--- Program.cs
+++ Program.cs
@@ -1,18 +1,47 @@
 ﻿using fundamentosdotnet.Models;
 
-int soma = 0, numero = 0;
 
-do
+
+string opcao;
+bool exibirmenu = true;
+
+
+while(exibirmenu)
 {
-   Console.WriteLine("Digite um numero");
-   numero = Convert.ToInt32(Console.ReadLine());
+   Console.Clear();
+   Console.WriteLine("Digite a sua opção");
+   Console.WriteLine("1 - Cadastrar cliente");
+   Console.WriteLine("2 - Buscar Cliente");
+   Console.WriteLine("3 - Apagar Cliente");
+   Console.WriteLine("4 - Encerrar");
 
-   soma += numero;
+   opcao = Console.ReadLine();
+
+   switch(opcao)
+   {
+      case "1":
+         Console.WriteLine("Cadastro de cliente");
+         break;
+
+      case "2":
+         Console.WriteLine("Busca de cliente");
+         break;
+      case "3":
+         Console.WriteLine("Apagar cliente");
+         break;
+      case "4":
+         Console.WriteLine("Encerrar");
+         //Environment.Exit(0);
+         break;   
+      default:
+      Console.WriteLine("Opção Inválida");
+         break;
+   }
+}
+
+Console.WriteLine("O programa se encerrou");
 
 
-} while (numero != 0);
-
-Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
 
 
@@ -25,6 +54,22 @@ Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
 
 
+
+
+
+// int soma = 0, numero = 0;
+
+// do
+// {
+//    Console.WriteLine("Digite um numero");
+//    numero = Convert.ToInt32(Console.ReadLine());
+
+//    soma += numero;
+
+
+// } while (numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
 
 
